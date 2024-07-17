7aa4e30a722b3699ce4a76c89f8863fff87401aa
glenndsilva1 <glenndsilva@rediffmail.com>
2024-07-17 14:16:13 +0530

    StringUtils.java

--- StringUtils.java
+++ StringUtils.java
@@ -1,17 +1,14 @@
 package util;
 
-public class StringUtils {
-	public static int lengthof(String a) throws Exception {
-		int strlength = 0;
-		if (a != null) {
-			strlength = a.length();
-		}
-		if (strlength == 0) {
-			throw new Exception("Str length not equal to zero");
-		}
-		return strlength;
-	}
+/*
+   Utility Classes have lot of issues take the example of the below code multiple clients can potentially call this method say tomorrow the length of minimum characters 
+   is changed to 20 then all the client code will have issues and we need to retest all the client code secondly if some of the clients still want to use say 15 we have to 
+   create another method to check those characters that will be a huge burder and all the more if this code is placed in a jar file the developer will not have a clue 
+   as he would not find it in normal search also utility classes are unbreakable and hard to test thats why they are a major threat to the code base and one should 
+   really be careful a good alternative is to use composible decorators 
+*/
 
+public class StringUtils {
 	public static boolean checkregid(String a) {
 		int strlength = 0;
 		boolean islength = false;
